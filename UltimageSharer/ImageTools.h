686115b39e66648689553e0fe16d25a2f76d5d28
apolotary <ryskeldiev.b@gmail.com>
2012-07-18 12:28:12 +0600

    first commit

---
+++ UltimageSharer/ImageTools.h
@@ -0,0 +1,13 @@
+//
+//  ImageTools.h
+//  UltimageSharer
+//
+//  Created by Bektur Ryskeldiev on 17/07/2012.
+//  Copyright (c) 2012 sibers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageTools : NSObject
+
+@end
---
+++ UltimageSharer/ImageTools.m
@@ -0,0 +1,13 @@
+//
+//  ImageTools.m
+//  UltimageSharer
+//
+//  Created by Bektur Ryskeldiev on 17/07/2012.
+//  Copyright (c) 2012 sibers. All rights reserved.
+//
+
+#import "ImageTools.h"
+
+@implementation ImageTools
+
+@end
